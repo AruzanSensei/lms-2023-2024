1da02cf06ebda85c95a3c38c38644ca07585dba7
Aruzan Sensei <alzanadytia.j@gmail.com>
2025-06-12 21:50:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import random
+import os
+port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)
 
@@ -878,4 +880,4 @@ def random_student(class_name):
         })
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=port)
